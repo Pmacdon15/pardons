ba934479fda519f3625670513610ef5eb2ab6857
Patrick MacDonald <patrick@patmac.ca>
2026-01-29 22:22:36 -0700

    added reply to

--- src/actions/resend.ts
+++ src/actions/resend.ts
@@ -1,40 +1,41 @@
-'use server'
-import { Resend } from 'resend'
-import type z from 'zod'
-import { EmailTemplate } from '@/components/email-templates/contact-email'
-import { formSchema } from '../zod/schemas'
+"use server";
+import { Resend } from "resend";
+import type z from "zod";
+import { EmailTemplate } from "@/components/email-templates/contact-email";
+import { formSchema } from "../zod/schemas";
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendEmail(values: z.infer<typeof formSchema>) {
-	const validatedFields = formSchema.safeParse({
-		name: values.name,
-		email: values.email,
-		subject:values.subject,
-		message: values.message,
-	})
+  const validatedFields = formSchema.safeParse({
+    name: values.name,
+    email: values.email,
+    subject: values.subject,
+    message: values.message,
+  });
 
-	if (!validatedFields.success) {
-		throw new Error(`Error sending email: invalid form`)
-	}
+  if (!validatedFields.success) {
+    throw new Error(`Error sending email: invalid form`);
+  }
 
-	const { data, error } = await resend.emails.send({
-		from: 'Contact Form <notifications@pardonsareus.ca>',
-		to: ['help@notifications.pardonsareus.ca'],
-		subject: 'Contact Form Filled Out',
-		react: EmailTemplate({
-			name: validatedFields.data.name,
-			email: validatedFields.data.email,
-			subject: validatedFields.data.subject,
-			message: validatedFields.data.message,
-		}),
-	})
+  const { data, error } = await resend.emails.send({
+    from: "Contact Form <notifications@pardonsareus.ca>",
+    to: ["help@notifications.pardonsareus.ca"],
+    subject: "Contact Form Filled Out",
+    react: EmailTemplate({
+      name: validatedFields.data.name,
+      email: validatedFields.data.email,
+      subject: validatedFields.data.subject,
+      message: validatedFields.data.message,
+    }),
+    replyTo: validatedFields.data.email,
+  });
 
-	console.log('Result: ', data)
+  console.log("Result: ", data);
 
-	if (error) {
-		console.error('Error ', error)
-		throw new Error(`Error sending email: ${JSON.stringify(error)}`)
-	}
-	return data
+  if (error) {
+    console.error("Error ", error);
+    throw new Error(`Error sending email: ${JSON.stringify(error)}`);
+  }
+  return data;
 }
