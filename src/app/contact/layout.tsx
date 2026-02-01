c42d802bb86319529c73c2744284561d85a58414
Patrick MacDonald <patrick@patmac.ca>
2026-02-01 11:23:28 -0700

    removed comments

--- src/app/contact/layout.tsx
+++ src/app/contact/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
-// import "./globals.css";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata: Metadata = {
   title: "Pardons Are Us | Canadian Record Suspension Assistance",
